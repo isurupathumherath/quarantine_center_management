b9356f6b79f2c21cdc620035d36883c5e319fb00
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2021-09-11 01:55:44 +0530

    updated App.js

--- Quarantine-Center-Management/webadmin/src/App.js
+++ Quarantine-Center-Management/webadmin/src/App.js
@@ -1,56 +1,30 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-//pages
-import viewTickets from './components/TicketManagement/adminAllTickets';
-import replyTickets from './components/TicketManagement/adminEditTickets';
-
-export default class App extends Component {
-
-  render() {
-    return (
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 //common navigation components
-import Header from './components/Common/Navigation/Header';
-import Sidebar from './components/Common/Navigation/Sidebar'; 
-
-//component
-import Login from './components/exampleComponent/Loginform';
+import Header from "./components/Common/Navigation/Header";
+import Footer from "./components/Common/Navigation/Footer";
 
 //pages
+import Login from "./pages/Common/Loginexaple";
+
 class App extends Component {
   render() {
     return (
       <Router>
         <Header />
-        <Sidebar />
-        <div class="page-wrapper">
-          <div class="content container-fluid">
+        <div class="content">
+          <div class="container-fluid">
             <Switch>
               <Route path="/login" exact component={Login} />
-
             </Switch>
           </div>
         </div>
+        <Footer />
       </Router>
-    )
+    );
   }
 }
 
-      <BrowserRouter>
-        <div className="container">
-
-          <Route path="/" exact component={viewTickets}></Route>
-          <Route path="/edit/:id" component={replyTickets}></Route>
-
-        </div>
-      </BrowserRouter>
-
-
-    )
-  }
-}
+export default App;
