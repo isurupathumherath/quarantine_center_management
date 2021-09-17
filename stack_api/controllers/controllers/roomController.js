3d4f08916d2cc81464a87f579ff9feb0e327a93b
roshani226 <86221628+roshani226@users.noreply.github.com>
2021-09-17 23:58:27 +0530

    roomControllers

---
+++ stack_api/controllers/controllers/roomController.js
@@ -0,0 +1,136 @@
+const express = require('express')
+var router = express.Router()
+var ObjectID= require('mongoose').Types.ObjectId
+var multer = require('multer')
+var uniqid = require('uniqid')
+
+var { room } = require('../models/room')
+var { booking } = require('../models/booking')
+
+router.get('/',(req,res)=>{
+    room.find((err,docs)=>{
+        if(!err){
+            res.send(docs)
+        }else{
+            console.log(JSON.stringify(err,undefined,2))
+        }
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send(req.params.id)
+    }
+
+    room.findById(req.params.id,(err,docs)=>{
+        if(!err){
+            res.send(docs)
+        }else{
+            console.log(JSON.stringify(err,undefined,2))
+        }
+    })
+})
+
+router.post('/',(req,res)=>{
+    var newRecord= new room({
+        roomName: req.body.roomName,
+        roomType: req.body.roomType,
+        description: req.body.description,
+        image: req.body.image,
+        total: req.body.total,
+    })
+
+    newRecord.save((err,docs)=>{
+        if(!err){
+            res.send(docs)
+        }else{
+            console.log(JSON.stringify(err,undefined,2))
+        }
+    })
+})
+
+router.post('/booking',(req,res)=>{
+    var record= new booking({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        nic: req.body.nic,
+        email: req.body.email,
+        checkin: req.body.checkin,
+        checkout: req.body.checkout,
+        roomid: req.body.roomid,
+        patientid: req.body.patientid,
+        price: req.body.price
+    })
+
+    record.save((err,docs)=>{
+        if(!err){
+            res.send(docs)
+        }else{
+            console.log(JSON.stringify(err,undefined,2))
+        }
+    })
+})
+
+router.put('/:id',(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send(req.params.id)
+    }
+
+    var updateRecords={
+        roomName: req.body.roomName,
+        roomType: req.body.roomType,
+        description: req.body.description,
+        image: req.body.image,
+        total: req.body.total,
+    }
+
+    room.findByIdAndUpdate(req.params.id, { $set: updateRecords},{new:true}, (err,docs)=>{
+        if(!err){
+            res.send(docs)
+        }else{
+            console.log(JSON.stringify(err,undefined,2))
+        }
+    })
+})
+
+router.delete('/:id',(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(400).send(req.params.id)
+    }
+
+    room.findByIdAndRemove(req.params.id,(err,docs)=>{
+        if(!err){
+            res.send(docs)
+        }else{
+            console.log(JSON.stringify(err,undefined,2))
+        }
+    })
+})
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, uniqid() + '-' +file.originalname )
+  }
+})
+
+var upload = multer({ storage: storage }).single('file')
+
+router.post('/upload',function(req, res) {
+     
+    upload(req, res, function (err) {
+           if (err instanceof multer.MulterError) {
+               return res.status(500).json(err)
+           } else if (err) {
+               return res.status(500).json(err)
+           }
+      return res.status(200).send(req.file)
+
+    })
+
+})
+
+module.exports = router
