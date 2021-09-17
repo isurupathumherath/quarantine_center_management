1abe611af258200f74a7f1492a4924c4d771c655
roshani226 <86221628+roshani226@users.noreply.github.com>
2021-09-18 00:18:48 +0530

    db.js

--- stack_api/db.js
+++ stack_api/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/room_m',{useNewUrlParser:true,useUnifiedTopology:true},
+ mongoose.connect('mongodb+srv://Member_1:IMLLDUKg6ReJniIi@cluster0.4vex8.mongodb.net/QMC_DB?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true},
 err => {
     if(!err){
         console.log("connection success!")
