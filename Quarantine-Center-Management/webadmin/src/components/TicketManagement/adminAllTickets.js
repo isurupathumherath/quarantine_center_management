d8c3d529420c5a8f7a9bb170ced5ee06c1e6448b
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2021-10-15 12:13:57 +0530

    alignment change - all ticket

--- Quarantine-Center-Management/webadmin/src/components/TicketManagement/adminAllTickets.js
+++ Quarantine-Center-Management/webadmin/src/components/TicketManagement/adminAllTickets.js
@@ -74,13 +74,13 @@ export default class adminAllTickets extends Component {
     return (
 
       // Table
-      <div className="card" style={{ padding: '40px', paddingTop: '50px', width:'1200px' ,boxShadow: '10px 10px 18px #888888' }}>
+      <div className="card" style={{ padding: '40px', paddingTop: '50px', width: '1500px', boxShadow: '10px 10px 18px #888888' }}>
         <div className="container">
           <div className="row">
             <div className="col-lg-9 mt-2 mb-2">
               <h3>All Tickets</h3>
               <br></br>
-              <hr style={{width:'1100px'}}></hr>
+              <hr style={{ width: '1100px' }}></hr>
               &nbsp;
               <ReactHTMLTableToExcel
                 className='btn btn-outline-success'
@@ -99,9 +99,9 @@ export default class adminAllTickets extends Component {
                 onChange={this.handleSearchArea} />
             </div>
           </div>
-          <div className="card">
-            <div className="table-responsive" style={{ maxHeight: "500px", overflowY: "scroll", }}>
-              <table id="enrollment-table" className="table table-hover" style={{ marginTop: '40px', width:'1500px' }}>
+          <div className="card" >
+            <div className="table-responsive" style={{ maxHeight: "600px", overflowY: "scroll" }}>
+              <table id="enrollment-table" className="table table-hover" style={{ marginTop: '40px', width: '1500px' }}>
                 <thead>
                   <tr>
                     <th style={{ width: '10px' }} scope="col">#</th>
@@ -110,7 +110,7 @@ export default class adminAllTickets extends Component {
                     <th style={{ width: '150px' }} scope="col">NIC</th>
                     <th style={{ width: '150px' }} scope="col">Email</th>
                     <th style={{ width: '100px' }} scope="col">Department Name</th>
-                    <th  scope="col">Message</th>
+                    <th scope="col">Message</th>
                     <th style={{ width: '300px' }} scope="col">Status</th>
                     <th style={{ width: '200px' }} scope="col">Reply</th>
                     <th style={{ width: '500px' }} scope="col">Action</th>
