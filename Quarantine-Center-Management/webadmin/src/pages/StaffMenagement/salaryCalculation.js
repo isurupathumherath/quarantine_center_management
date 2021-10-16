9d6338e727096e08574995acac80e3ca941c0d25
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-10-16 16:01:11 +0530

    Salary Update

--- Quarantine-Center-Management/webadmin/src/pages/StaffMenagement/salaryCalculation.js
+++ Quarantine-Center-Management/webadmin/src/pages/StaffMenagement/salaryCalculation.js
@@ -17,31 +17,34 @@ const App = () => {
     const [wordEntered, setWordEntered] = useState("");
     const [count, setCount] = useState([]);
 
+    // state
+    const [state, setState] = useState({
+        startedDate: '',
+        endDate: '',
+    });
+
+    //destructure values from state
+    const {
+        startedDate, endDate
+    } = state;
+
+    function handleChange(name) {
+        return function (event) {
+            setState({ ...state, [name]: event.target.value });
+        };
+    }
+
     //Fetch All staff Members
     const fetchStaffMembers = () => {
-        axios.get("http://localhost:8000/qEmployee/showAll")
+        startedDate = startedDate;
+        endDate = endDate;
+        axios.get(`http://localhost:8000/salary/get-salary-range/${startedDate}/${endDate}`)
             .then(response => {
                 console.log(response)
                 setStaffMembers(response.data)
-                setCount(response.data.length);
+                // setCount(response.data.length);
             })
             .catch(error => alert("Error Fetching Staff Members"));
-    }
-
-    //Delete staff Member by ID
-    const deleteQStaff = (employeeId) => {
-        axios
-            .delete(`http://localhost:8000/qEmployee/remove/${employeeId}`)
-            .then(response => {
-                // alert(response.data.message);
-                Swal.fire(
-                    `Staff Member ${employeeId} is Removed`,
-                    `${response.data.message}`,
-                    'success'
-                )
-                fetchStaffMembers();
-            })
-            .catch(error => alert('Error deleting Staff Member'));
     }
 
     //Filter Staff Member
@@ -58,7 +61,7 @@ const App = () => {
 
                 if (searchWord === "") {
                     console.log("EMPLTY");
-                    fetchStaffMembers();
+                    // fetchStaffMembers();
                 } else {
                     setStaffMembers(newFilter);
                 }
@@ -67,121 +70,28 @@ const App = () => {
     };
 
     useEffect(() => {
-        fetchStaffMembers();
+        // fetchStaffMembers();
     }, [])
 
     return (
         <div style={{ marginLeft: "50px" }}>
             <div className="card" style={{ width: "1300px" }}>
                 <div className="card-body">
-                    <h1 align="center">Quarantined Staff Members</h1>
+                    <h1 align="center">Salary Calculation</h1>
                     <br />
-                    <div>
-                        <center>
-                            <div
-                                className="border border-info"
-                                style={{
-                                    width: "100%",
-                                    backgroundColor: "white",
-                                    borderRadius: "10px",
-                                    borderColor: "#00408C",
-                                    padding: "20px 20px 20px 20px",
-                                    margin: "10px 0px 0px 0px",
-                                    align: "center"
-                                }}
-                            >
-                                <div className="row">
-                                    <div className="col">
-                                        <span style={{ color: "blue" }}><h3>{count}</h3></span>
-                                        <span><h3>Number of Quarantine Staff Members</h3></span>
-                                    </div>
-                                    {/* <div className="col">
-                                        <i
-                                            class="fa  fa-hourglass-end"
-                                            aria-hidden="true"
-                                            style={{
-                                                color: "blue",
-                                                fontSize: "30px",
-                                                marginTop: "10px",
-                                            }}
-                                        ></i>
-                                    </div> */}
-                                </div>
+                    <form onSubmit={fetchStaffMembers}>
+                        <td>
+                            <div className="form-group">
+                                <input type="date" className="form-control" onChange={handleChange('startedDate')} value={startedDate} required />
                             </div>
-                            <form style={{ marginTop: '40px', marginLeft: '20px', marginRight: '40px', width: "100%" }}>
-                                <div className="col-lg-3 mt-2 mb-2">
-                                    <input
-                                        className="form-control"
-                                        type="search"
-                                        placeholder="Search"
-                                        value={wordEntered}
-                                        onChange={handleFilter}
-                                    />
-                                </div>
-                            </form>
-                        </center>
-                    </div>
-                    <div>
-
-                    </div>
-
-
-
-                    <table id="table" class="table" responsive className="table table-hover" style={{ marginTop: '40px', marginLeft: '20px', width: '95%' }}>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Employee ID</th>
-                                <th>From</th>
-                                <th>To</th>
-                                <th>Added At</th>
-
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {staffMembers.map((staffMembers, i) => (
-                                <tr key={i}>
-                                    <th scope="row">{i + 1}</th>
-
-                                    <a href={`/singleProfile/${staffMembers.employeeId}`} style={{ textDecoration: 'none' }}>
-                                        <td>{staffMembers.employeeId}</td>
-                                    </a>
-
-                                    <td>{moment(staffMembers.startedDate).format('MMMM Do YYYY, h:mm:ss a')}</td>
-                                    <td>{moment(staffMembers.endDate).format('MMMM Do YYYY, h:mm:ss a')}</td>
-                                    <td>{moment(staffMembers.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</td>
-
-                                    <td>
-                                        {/* <a className="" href={`/updateStaffMember/${staffMembers.employeeId}`}>
-                                            <i className="fas fa-edit"></i>&nbsp;
-                                        </a>
-                                        &nbsp; */}
-                                        <a className="" href="#" onClick={() => deleteQStaff(staffMembers.employeeId)}>
-                                            <i className="far fa-trash-alt"></i>&nbsp;
-                                        </a>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                    <br />
-                    <a className="btn btn-primary btn-lg btn-block" href={`/addQuaratineStaff/`}>
-                        Add a New Quarantine Staff Member
-                    </a>
-
-                    <br />
-                    <div >
-                        <ReactHTMLTableToExcel
-                            className='btn btn-outline-primary btn-lg btn-block'
-                            table='table'
-                            filename='Quarantined Staff Member Excel'
-                            sheet='Sheet'
-                            buttonText='Download Excel Sheet'
-                        />
-
-                    </div>
-
+                        </td>
+                        <td>
+                            <div className="form-group">
+                                <input type="date" className="form-control" onChange={handleChange('endDate')} value={endDate} required />
+                            </div>
+                        </td>
+                        <td><input type="submit" value="Submit" /></td>
+                    </form>
                 </div>
             </div>
         </div >
